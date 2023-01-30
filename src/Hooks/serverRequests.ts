063f7ae06f14d9f796390360189ef7ae74a1838b
marinawex <sheep803@gmail.com>
2023-01-30 23:25:44 +0200

    final

--- src/Hooks/serverRequests.ts
+++ src/Hooks/serverRequests.ts
@@ -1,4 +1,4 @@
-const endpoint = "http://localhost:3333";
+const endpoint = 'http://localhost:3333';
 
 export const startGame = async () => {
   const res = await fetch(`${endpoint}/getWord`);
@@ -10,22 +10,16 @@ export const sendWordToTheServer = async (guess: Array<object>) => {
   const usersGuess = JSON.stringify(guess);
   try {
     const response = await fetch(`${endpoint}/checkWord`, {
-      method: "post",
+      method: 'post',
       body: usersGuess,
       headers: {
-        "content-type": "application/json",
+        'content-type': 'application/json',
       },
     });
     const responseArray = await response.json();
 
     return responseArray;
   } catch (error) {
-    console.error("Error:", error);
+    console.error('Error:', error);
   }
-};
-
-export const checkWin = async () => {
-  const response = await fetch(`${endpoint}/checkWin`);
-  const isWin = await response.json();
-  return await isWin.win;
 };
